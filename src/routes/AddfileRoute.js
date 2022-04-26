b0a23648da7326a50dda859cd6021aa7227d0fb8
AhmedYounsi <hammuoa07@gmail.com>
2022-04-26 15:04:26 +0100

    sdf

--- src/routes/AddfileRoute.js
+++ src/routes/AddfileRoute.js
@@ -27,6 +27,7 @@ const upload = multer({
  
  
 router.post('/',upload.single("file"), async (req, res) => { 
+  console.log(req.file)
 if(req.file.filename) return res.status(200).send(req.file.filename)
 });
  
